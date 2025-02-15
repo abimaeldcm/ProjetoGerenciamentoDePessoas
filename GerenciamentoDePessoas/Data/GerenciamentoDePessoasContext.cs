f427c400a784c51b8ee039713dfce08c0dee8fee
abimaelMeta <abimael.mendes@metanetsistemas.com.br>
2025-02-14 22:23:56 -0300

    Adicionar arquivos de projeto.

---
+++ GerenciamentoDePessoas/Data/GerenciamentoDePessoasContext.cs
@@ -0,0 +1,13 @@
+﻿using GerenciamentoDePessoas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciamentoDePessoas.Data
+{
+    public class GerenciamentoDePessoasContext : DbContext
+    {
+        public GerenciamentoDePessoasContext(DbContextOptions<GerenciamentoDePessoasContext> options) : base(options)
+        {
+        }
+        public DbSet<Pessoas> Pessoas { get; set; }
+    }
+}
---
+++ GerenciamentoDePessoas/Models/Enum/ETipoSanguineo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciamentoDePessoas.Models.Enum
+{
+    public enum ETipoSanguineo
+    {
+        APositivo,     // A+
+        ANegativo,     // A-
+        BPositivo,     // B+
+        BNegativo,     // B-
+        ABPositivo,    // AB+
+        ABNegativo,    // AB-
+        OPositivo,     // O+
+        ONegativo      // O-
+    }
+}
